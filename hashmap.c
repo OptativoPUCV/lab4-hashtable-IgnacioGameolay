7017b9aa7183cf7ae24f495a8d110ca8b73d6850
Ignacio Antonio Reyes Toledo <45189062+IgnacioGameolay@users.noreply.github.com>
2024-04-25 04:14:42 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -112,14 +112,24 @@ void eraseMap(HashMap * map,  char * key) {
 
 Pair * searchMap(HashMap * map,  char * key) {   
   int position = hash(key,map->capacity);
-
-  if (is_equal(map->buckets[position]->key,key)){
-    map->current = position;
-    return map->buckets[position];
+  
+  while (map->buckets[position] != NULL) {
+    if (is_equal(map->buckets[position]->key,key)){
+      map->current = position;
+      return map->buckets[position];
+    }
+    
+    position = (position + 1) % map->capacity;
+    if (position == hash(key, map->capacity)) {
+      break;
+    }
   }
-
+  
     return NULL;
 }
+
+
+
 
 Pair * firstMap(HashMap * map) {
   
@@ -129,6 +139,7 @@ Pair * firstMap(HashMap * map) {
       return map->buckets[i];
     }
   }
+  
   map->current = -1;
   return NULL;
 }
