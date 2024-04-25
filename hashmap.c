e6084917ce9db6e2a1b92a00bf53dee9bc6165b0
Ignacio Antonio Reyes Toledo <45189062+IgnacioGameolay@users.noreply.github.com>
2024-04-25 02:41:48 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -117,7 +117,7 @@ Pair * searchMap(HashMap * map,  char * key) {
 Pair * firstMap(HashMap * map) {
   
   for (int i = 0; i < map->capacity; i++){
-    if (map->buckets[i] != NULL){
+    if ((map->buckets[i] != NULL) && (map->buckets[i]->key != NULL)){
       map->current = i;
       return map->buckets[i];
     }
